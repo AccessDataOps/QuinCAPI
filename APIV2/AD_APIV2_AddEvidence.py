5d3c8a479ae472d91b568d3960d8b3a9775010bd
TodEwasko <tewasko@accessdata.com>
2019-03-21 13:03:46 -0400

    Update AD_APIV2_AddEvidence.py

--- APIV2/AD_APIV2_AddEvidence.py
+++ APIV2/AD_APIV2_AddEvidence.py
@@ -4,9 +4,15 @@ import json
 
 targets = '192.168.9.205'
 caseid = 2
+servername = '192.168.9.205'
+Enterprisekey = 'b4cfe8e6-3ee9-4663-8e84-516fa890de73'
+evidencepath = '\\\\192.168.9.205\\Data\\text'
 
-headers = {'EnterpriseApiKey': 'b4cfe8e6-3ee9-4663-8e84-516fa890de73'}
-evidence = {'evidenceToCreate': { 'evidencePath': '\\\\192.168.9.205\\Data\\OST' }}
-resp = requests.post('http://192.168.9.205:4443/api/v2/enterpriseapi/'+str(caseid)+'/processdata',json = evidence,headers=headers)
+headers = {'EnterpriseApiKey': Enterprisekey }
+evidence = {'evidenceToCreate': { 'evidencePath': evidencepath }}
+resp = requests.post('http://'+servername+':4443/api/v2/enterpriseapi/'+str(caseid)+'/processdata',json = evidence,headers=headers)
 print(resp)
 print(resp.content)
+#This is what a good response looks like:
+#<Response [200]>
+#b'[77]'
