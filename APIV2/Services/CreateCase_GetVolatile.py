0356e33ca399db34071d1471f9a35f420e5d1bb1
elmerohueso <elmerohueso@users.noreply.github.com>
2019-04-24 10:51:40 -0600

    Update CreateCase_GetVolatile.py
    Added reports
    Does Processes, Services, and Network Sockets

--- APIV2/Services/CreateCase_GetVolatile.py
+++ APIV2/Services/CreateCase_GetVolatile.py
@@ -1,3 +1,14 @@
+# This script will do the following:
+# 1. Prompt for a case name
+# 2. Create the case
+# 3. Prompt for a target
+# 4. Collect volatile data from that target
+# 5. Rename the XML files with the results, for outside viewing
+# 6. Import the collected volatile data into the case
+#
+# Make sure to update the APIkey, APIhostname, and ProjectData
+# The account running this script must have full access to the path in ProjectData
+
 import requests
 import base64
 import json
@@ -6,10 +17,12 @@ import os
 import chardet
 import time
 from datetime import datetime
+from shutil import copyfile
 
-# API connection info
+# UPDATE THESE
 APIkey = "6400ddd5-6419-450a-b96c-a568f5ea309b" # Generated in Enterprise via Tools > Access API Key
 APIhostname = "WIN-B3VKJBVM6RQ" # Machine (name or IP) running Enterprise and Quin-C Self Host Service
+ProjectData = "\\\\WIN-B3VKJBVM6RQ\\AccessData\\ProjectData" # Default case data path, make sure to escape any backslashes
 
 headers = {'EnterpriseApiKey': APIkey}
 
@@ -23,12 +36,9 @@ except ConnectionError: # Nicer failure message than a full traceback
     print("Check the 'Quin-C Self Host Service' on %s" % APIhostname)
     raise SystemExit
 
-# Default case data paths
-ProjectData = "\\\\WIN-B3VKJBVM6RQ\\AccessData\\ProjectData"
-JobData = "\\\\WIN-B3VKJBVM6RQ\\AccessData\\JobData"
-
 # Prompt for a case name
 print()
+print("Case data will be stored at '%s'" % ProjectData)
 CaseName = input("Enter your desired case name: ")
 
 # Create the case and get the CaseID
@@ -36,7 +46,11 @@ CaseName = input("Enter your desired case name: ")
 print()
 print("Creating case '%s'..." % CaseName)
 ProjectData = ProjectData + "\\" + CaseName
-JobData = JobData + "\\" + CaseName
+Reports = ProjectData + "\\Reports"
+JobData = ProjectData + "\\JobData"
+
+if not os.path.exists(Reports):
+    os.makedirs(Reports)
 
 if not os.path.exists(ProjectData):
     os.makedirs(ProjectData)
@@ -52,9 +66,11 @@ createcaseDefinition = {
 }
 
 resp = requests.post("http://" + APIhostname + ":4443/api/v2/enterpriseapi/createcase",createcaseDefinition,headers=headers)
-print("Success")
+print(status.reason)
+print()
 CaseID = resp.content.decode("utf-8")
-print("Case ID = %s" % CaseID)
+print("Case ID: %s" % CaseID)
+print("Project folder: %s" % ProjectData)
 
 # Prompt you for a target IP
 # TODO: Can we do comma separated values, hostname, IP range, host name, or import a list from  a TXT file?
@@ -63,11 +79,14 @@ print("Case ID = %s" % CaseID)
 print()
 targets = input("Enter the desired target's IP: ")
 
-# Grab Services
+# Grab Processes, Services, Network Sockets
 # TODO: include hidden processes, DLLs (including injected), Sockets, Handles, Services, Drivers, Users, and Network info
 volatileDefinition = {
   "volatile": {
     "includeProcessTree": True,
+    "processTreeOptions": {
+      "includeSockets": True
+    },
     "includeServices": True
   },
   "ips": {
@@ -94,21 +113,33 @@ while (data["state"] == "InProgress") or (data["state"] == "Submitted"):
 
 print("Job %s is %s." % (JobID,data["state"]))
 
-# List the XML files containing the job results
-if (data["state"] == "Completed"):
-    print("Results:")
-    result = json.loads(data["resultData"])
-    realdata = json.loads(result["RealData"])
-    for i in range(0,len(realdata["TaskStatusList"][0]["Results"])):
-        print(realdata["TaskStatusList"][0]["Results"][1]["ResultFileLocation"][i]["FilePath"])
+# Parse the ResultFiles section of the output, and the target
+resultfiles = json.loads(json.loads(json.loads(data["resultData"])["RealData"])["TaskStatusList"][0]["Results"][1]["Data"])["ResultFiles"]
+target = json.loads(json.loads(json.loads(data["resultData"])["RealData"])["TaskStatusList"][0]["Results"][0]["Data"])["taskStatus"]["Connection"]
 
-# TODO: Output to XLS in a tabular format by IP and or info.
-# Import into ENT GUI so the cust had historical data.
+# Build a list of the result XML files
+tasks = []
+for i in range(0,len(resultfiles)):
+    if (resultfiles[i]["Filename"] != "certificates.xml"): # We don't care about this file
+        tasks.append({"Target": target, "Path": resultfiles[i]["Path"], "Filename": resultfiles[i]["Filename"]})
+
+# Copy the result XML files to a nicer location and name
+starttime = datetime.strptime(data["startDate"], '%Y-%m-%dT%H:%M:%S.%f')
+for i in range(0,len(tasks)):
+    src = tasks[i]["Path"]
+    dst = "%s\\%s_%s_%s" % (Reports,tasks[i]["Target"],starttime.strftime("%Y%m%d %H%M%SUTC"),tasks[i]["Filename"])
+    copyfile(src,dst)
+
+print()
+print("XML reports can be found in '%s'" % Reports)
+print("These can be opened with Excel.")
+
+# Import collected data into the case
 print()
 print("Adding volatile data to the case")
 resp = requests.get("http://" + APIhostname + ":4443/api/v2/enterpriseapi/"+str(CaseID)+"/importvolatile/"+str(JobID),headers=headers)
-print(resp)
-print(resp.content)
+print(status.reason)
+
 
 # Ability to schedule scans in the script. Run X times every X mins/secs/hours
 # Would be cool if we could perform a delta of the scans automatically.
