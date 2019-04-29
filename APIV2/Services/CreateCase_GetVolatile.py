7b7825215f00c91b8d48f22b9740d42964bf771a
elmerohueso <elmerohueso@users.noreply.github.com>
2019-04-29 12:59:51 -0600

    agent check

--- APIV2/Services/CreateCase_GetVolatile.py
+++ APIV2/Services/CreateCase_GetVolatile.py
@@ -1,3 +1,6 @@
+# Version: .4, check target listening, report job failure, pause at the end
+# Date: 4/29/2019
+#
 # This script will do the following:
 # 1. Prompt for a case name
 # 2. Create the case
@@ -6,8 +9,8 @@
 # 5. Rename the XML files with the results, for outside viewing
 # 6. Import the collected volatile data into the case
 #
-# EntAPICommon.py must reside in the same folder as this script
-#
+# IMPORTANT:
+# EntAPICommon.py must reside in the same folder as this script.
 # Make sure to update the APIkey, APIhostname, and ProjectData
 # The account running this script must have full access to the path in ProjectData
 
@@ -28,6 +31,7 @@ if EntAPICommon.IsApiUp(APIhostname):
 else:
     print("Failed")
     print("Check the 'Quin-C Self Host Service' on %s" % APIhostname)
+    os.system("pause")
     raise SystemExit
 
 # Prompt for a case name
@@ -65,10 +69,15 @@ print("Project folder: %s" % ProjectData)
 
 # Prompt you for a target IP
 # TODO: Can we do comma separated values, hostname, IP range, host name, or import a list from  a TXT file?
-# TODO: Check if target is listening
 # TODO: Push the Agent
 print()
 targets = input("Enter the desired target's IP: ")
+
+# Check if 3999 is listening on target
+if not EntAPICommon.IsPortListening(str(targets), 3999):
+  print("Target isn't responding on port 3999. Check Agent service on '%s'" % str(targets))
+  os.system("pause")
+  raise SystemExit
 
 # Grab Processes, Services, Network Sockets
 # TODO: include hidden processes, DLLs (including injected), Sockets, Handles, Services, Drivers, Users, and Network info
@@ -99,7 +108,18 @@ while (state in ["InProgress", "Submitted"]):
   time.sleep(60)
   jobinfo = EntAPICommon.GetJobInfo(APIkey, APIhostname, CaseID, JobID)
   state = jobinfo["state"]
-print("Job %s is %s." % (JobID,state))
+try:
+  RealState = jobinfo["resultData"]["RealData"]["State"]
+except KeyError:
+  print("Job %s experienced an error and cannot proceed." % JobID)
+  os.system("pause")
+  raise SystemExit
+if RealState == "Finished":
+  print("Job %s is %s." % (JobID,state))
+else:
+  print("Job %s experienced an error and cannot proceed." % JobID)
+  os.system("pause")
+  raise SystemExit
 
 # Get the Target Name and list of ResultFiles from the job info
 target = jobinfo["resultData"]["RealData"]["TaskStatusList"][0]["Details"]["Name"]
@@ -127,6 +147,7 @@ print()
 print("Adding volatile data to the case")
 print(EntAPICommon.AddVolatile(APIkey, APIhostname, CaseID, JobID))
 
+os.system("pause")
 
 # Ability to schedule scans in the script. Run X times every X mins/secs/hours
 # Would be cool if we could perform a delta of the scans automatically.
