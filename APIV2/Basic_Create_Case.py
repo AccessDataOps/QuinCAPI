4ae8585100e147fceabbab97efc2793b6e2ec788
elmerohueso <elmerohueso@users.noreply.github.com>
2019-05-30 13:34:31 -0600

    Create Basic_Create_Case.py

---
+++ APIV2/Basic_Create_Case.py
@@ -0,0 +1,16 @@
+import requests
+
+apihost = "WIN-B3VKJBVM6RQ"
+apikey = "c45a22c3-8da7-49ee-929b-ab6185fa90e4"
+casename = "API Script Case"
+casepath = "\\\\WIN-B3VKJBVM6RQ\\AccessData\\ProjectData"
+
+headers = {'EnterpriseApiKey': apikey}
+createcase = {
+"Name": casename,
+"ProcessingMode":2,
+"FTKCaseFolderPath": casepath
+}
+
+resp = requests.post('http://'+apihost+':4443/api/v2/enterpriseapi/createcase',createcase,headers=headers)
+print('Created case %s' % resp.text)
