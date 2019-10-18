56652e02a718e0a43e43d8984805b904ddccdc13
IvanNebogatikov <ivan.nebogatikov@gmail.com>
2019-10-18 19:54:21 +0300

    Added relaivity

--- APIV2/SearchCountAndExportCombined.py
+++ APIV2/SearchCountAndExportCombined.py
@@ -21,6 +21,9 @@ statuscheckresp = requests.get('http://'+servername+':4443/api/v2/enterpriseapi/
 print("Checking Status of API... " + statuscheckresp.reason)
 #TODO add in if not OK, exit here
 
+with open('Relativity.json', 'r', encoding='utf-8-sig') as file:
+    relatility = json.load(file)
+
 top = tkinter.Tk()
 top.title("Automate AD LAB 7.1. Search Count Report & Export")
 top.geometry("400x350")
@@ -55,8 +58,8 @@ folderPath.grid(column = 0, row = 5)
 lblExport = Label(master=top,text="Export type", width = 50, height = 2, anchor = S)
 lblExport.grid(row=6, column=0)
 selectedType = StringVar(top)
-selectedType.set("Ad1") # default value
-typeMenu = OptionMenu(top, selectedType, "Ad1", "Native", "LoadFile", "ImageLoadFile", "ProductionSet") #, "BulkPrintJob")
+selectedType.set("Relativity") # default value
+typeMenu = OptionMenu(top, selectedType, "Ad1", "Native", "LoadFile", "ImageLoadFile", "ProductionSet", "Relativity") #, "BulkPrintJob")
 typeMenu.grid(row=7, column=0)
 
 lblStatus = tkinter.Text(top, width = 40, height = 40, state=DISABLED)
@@ -67,7 +70,13 @@ def clickedSubmit():
     lblStatus.configure(state='normal')
     lblStatus.delete('1.0', END)
     lblStatus.configure(state='disabled')
+    
     caseId = int(caseText.get())
+    caseResp = requests.get(f'http://{servername}:4443/api/v2/enterpriseapi/core/getcaselist',headers = headers)
+    jsonCaseResp = list(json.loads(caseResp.content))
+    casePath = next((case for case in jsonCaseResp if str(case['id']) == str(caseId)), None)['casePath']
+    reportPath = os.path.join(casePath, 'SearchReports')
+
     csvFilePath = str(csvPath.get())
     exportFolder = str(folderPath.get())
     with open(csvFilePath, 'r', encoding='utf-8-sig') as file:
@@ -99,6 +108,8 @@ def clickedSubmit():
     lblStatus.insert('1.0', f'\n')
     lblStatus.configure(state='disabled')
 
+    os.startfile(reportPath)
+
     labelIds = []
     for term in terms:
         lbl = {'Name' : term}
@@ -113,7 +124,13 @@ def clickedSubmit():
     lblStatus.configure(state='disabled')
     
     for lblId, lblName in labelIds:
-        req = {'ExportBase': {'ExportPath':  exportFolder, 'JobName' : jobName, 'Label' : {'Id' : str(lblId)}}, 'exportType' : selectedType.get()}
+        tp = selectedType.get()
+        if tp == "Relativity":
+            req = relatility
+            req['exportBase']['exportPath'] = exportFolder 
+            req['exportBase']['label'] = {'Id' : str(lblId)}
+        else:
+            req = {'ExportBase': {'ExportPath':  exportFolder, 'JobName' : jobName, 'Label' : {'Id' : str(lblId)}}, 'exportType' : selectedType.get()}
         respExport = requests.post(f'http://{servername}:4443/api/v2/enterpriseapi/jobs/{caseId}/createexportset', json = req, headers=headers)
         jobExport = int(json.loads(respExport.content)['jobId'])
         while True:
